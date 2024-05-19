eaae6039fc7792be5ffe86c935d9d2532102205c
Rinjee <rinjigelu39@gmail.com>
2024-05-19 12:56:51 -0600

    installed uuid, added id to the notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000 ;
@@ -22,7 +24,10 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
+    const newNote = {
+            id: uuidv4(),
+        ...req.body
+    };
     fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error(err);
